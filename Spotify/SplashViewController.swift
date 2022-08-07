48db706d13e34e656316f7bc241e78267fa94530
Gülzade Karataş <32436596+gulzade@users.noreply.github.com>
2022-08-07 07:07:36 +0300

    Add files via upload

---
+++ Spotify/SplashViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Spotify
+//
+//  Created by Gülzade Karataş on 7.08.2022.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
---
+++ Spotify/SpotifyPlayingScreen.swift
@@ -0,0 +1,21 @@
+//
+//  SpotifyPlayingScreen.swift
+//  Spotify
+//
+//  Created by Gülzade Karataş on 7.08.2022.
+//
+
+import UIKit
+
+class SpotifyPlayingScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    
+   
+    
+
+}
